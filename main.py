8cb9df1a180de5af64ece0bb3ece3735986054ac
Eduardo Schulz <eduardo.schulz@protonmail.com>
2025-06-14 11:55:47 -0300

    edit main

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from usuario import Usuario, Professor, Aluno
 from emprestimo import Emprestimo
 
 
+
+
 l1 = Livro("livro bom", "sla", 12, True)
 l1.exibirInformacoes()
 
@@ -11,3 +13,5 @@ u = Professor("Nome", 1, "TI")
 e = Emprestimo(l1, u, "1", "12")
 
 e.exibirResumo()
+
+
