49d9a4247bd0c4470852192d3e8acb07cc01750b
Eduardo Schulz <eduardo.schulz@protonmail.com>
2025-06-15 19:17:30 -0300

    add tipouser()

--- usuario.py
+++ usuario.py
@@ -5,20 +5,30 @@ class Usuario:
 
     def exibirInformacoes(self):
         print(f'Nome: {self.nome}\nID: {self.id}')
+        self.exibirTipoUsuario()
 
-    def exibirTipo(self):
-        print()
 
+    def exibirTipoUsuario(self):
+        print("Tipo: Usuário Genérico")
+ 
 class Professor(Usuario):
 
     def __init__(self, nome, id, departamento):
         super().__init__(nome, id) 
         self.departamento = departamento
+
+    def exibirTipoUsuario(self):
+        print("Tipo: Professor")
+        print(f"Departamento: {self.departamento}")
     
 class Aluno(Usuario):
 
     def __init__(self, nome, id, curso):
         super().__init__(nome, id) 
         self.curso = curso
+
+    def exibirTipoUsuario(self):
+        print("Tipo: Aluno")
+        print(f"Curso: {self.curso}")
 
   
